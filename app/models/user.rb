0a761e14100b2b7cd78468e4bd3d5b85b57df962
sumanp <sumanpuri55@gmail.com>
2024-02-15 15:44:05 +0530

    Favorites & classmates fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
+  has_many :enrollments
+  has_many :enrolled_programs, through: :enrollments, source: :program
+  has_many :teachers, through: :enrollments, source: :teacher
+
   enum :kind, [:teacher, :student, :student_teacher]
+
+  def self.classmates(user)
+    where.not(id: user.id).joins(:enrollments).where(enrollments: { program_id: user.enrolled_programs.pluck(:id) })
+  end
 end
